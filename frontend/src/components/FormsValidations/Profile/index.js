76f7d8a0f8ab1523ce06bb8f332d9c3e86ca4e82
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-10-20 22:44:31 +0530

    Delete frontend/src directory

--- frontend/src/components/FormsValidations/Profile/index.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup'
-
-export const profileSchema = Yup.object({
-    age:Yup.number().min(5).max(100).required("Please Enter Your age Name"),
-    number: Yup.number().required("Please Enter Your mobile Number"),
-    gender : Yup.string().required("Please Enter Your gender"),
-})
